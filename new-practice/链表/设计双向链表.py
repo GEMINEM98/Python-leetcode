9f829ec60802723d8b9f16dc83ec0fb6a72ae165
geliuyi <1225705207@qq.com>
2024-08-22 14:23:12 +0800

    测试绿点

--- new-practice/链表/设计双向链表.py
+++ new-practice/链表/设计双向链表.py
@@ -16,7 +16,6 @@ void addAtHead(int val) 将一个值为 val 的节点插入到链表中第一个
 void addAtTail(int val) 将一个值为 val 的节点追加到链表中作为链表的最后一个元素。
 void addAtIndex(int index, int val) 将一个值为 val 的节点插入到链表中下标为 index 的节点之前。如果 index 等于链表的长度，那么该节点会被追加到链表的末尾。如果 index 比长度更大，该节点将 不会插入 到链表中。
 void deleteAtIndex(int index) 如果下标有效，则删除链表中下标为 index 的节点。
-
 '''
 
 class ListNode:

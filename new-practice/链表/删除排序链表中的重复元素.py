e2edcb22a3d40dae75eb4e3f90215746386ff283
geliuyi <1225705207@qq.com>
2024-08-30 16:13:16 +0800

    链表-删除排序链表中的重复元素

--- new-practice/链表/删除排序链表中的重复元素.py
+++ new-practice/链表/删除排序链表中的重复元素.py
@@ -14,7 +14,53 @@ class ListNode:
 
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return head
+
+        left = head
+        right = head.next
+        while right:
+            if right.val != left.val:
+                left = left.next
+                left.val = right.val
+                left.next = right.next
+            right = right.next
+        left.next = None
+        return head
 
 
+def print_linked_list(head: Optional[ListNode]) -> None:
+    current = head
+    while current:
+        print(current.val, end=" -> " if current.next else "\n")
+        current = current.next
+
+
+def main():
+    # 创建链表：1 -> 1 -> 2 -> 3 -> 3
+    node1 = ListNode(1)
+    node2 = ListNode(1)
+    node3 = ListNode(2)
+    node4 = ListNode(3)
+    node5 = ListNode(3)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+
+    print("Original linked list:")
+    print_linked_list(node1)
+
+    # 删除重复元素
+    solution = Solution()
+    result_head = solution.deleteDuplicates(node1)
+
+    print("Linked list after removing duplicates:")
+    print_linked_list(result_head)
+
+
+if __name__ == "__main__":
+    main()
 
 
